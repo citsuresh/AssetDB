4715941b7c71bc14242a0d7886747c36ca71883f
Suresh Kumar Veluswamy <citsuresh@rediffmail.com>
2017-12-11 15:43:45 +0530

    Encrypted connection string

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,14 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Web.Configuration;
 
 public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+	    Configuration configuration = WebConfigurationManager.OpenWebConfiguration("~");
+	    ConnectionStringsSection connectionStringsSection = (ConnectionStringsSection) configuration.GetSection("connectionStrings");
+		connectionStringsSection.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
+		configuration.Save();
 
-    }
+		////< connectionStrings >
+		////< add name = "ApplicationServices" connectionString = "data source=tcp:assetdbazuresample.database.windows.net,1433;initial catalog=AssetDBAzureSample;user id=AssetDBAdmin@assetdbazuresample.database.windows.net;password=Mtts1000;MultipleActiveResultSets=False;Timeout=60" providerName = "System.Data.SqlClient" />
+		////< add name = "SQLConnectionString" connectionString = "data source=tcp:assetdbazuresample.database.windows.net,1433;initial catalog=AssetDBAzureSample;user id=AssetDBAdmin@assetdbazuresample.database.windows.net;password=Mtts1000;MultipleActiveResultSets=False;Timeout=60" providerName = "System.Data.SqlClient" />
+		////</ connectionStrings >
+	}
 }
