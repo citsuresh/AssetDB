032d242800d65f3a2bbafc34ce90e4868090463c
Suresh Kumar Veluswamy <citsuresh@rediffmail.com>
2017-12-13 12:08:35 +0530

    updated config

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -9,7 +9,7 @@ public partial class _Default : System.Web.UI.Page
 	    Configuration configuration = WebConfigurationManager.OpenWebConfiguration("~");
 	    ConnectionStringsSection connectionStringsSection = (ConnectionStringsSection) configuration.GetSection("connectionStrings");
 		connectionStringsSection.SectionInformation.ProtectSection("DataProtectionConfigurationProvider");
-		configuration.Save();
+		//configuration.Save();
 
 		////< connectionStrings >
 		////< add name = "ApplicationServices" connectionString = "data source=tcp:assetdbazuresample.database.windows.net,1433;initial catalog=AssetDBAzureSample;user id=AssetDBAdmin@assetdbazuresample.database.windows.net;password=Mtts1000;MultipleActiveResultSets=False;Timeout=60" providerName = "System.Data.SqlClient" />
